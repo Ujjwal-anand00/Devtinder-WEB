439decc47747bb84754bdb8e91e37963aac9ba48
Ujjwal Anand <ujjwal.anand6376@gmail.com>
2025-07-13 14:34:21 +0530

    Starting DevTinder Frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-8xl text-center text-amber-600">Hello World</h1>
+    </>
+  )
+}
+
+export default App
